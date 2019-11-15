f9eb5f178065c0691b4d5599d754694ca1711d18
DONGGUN LEE <d0n99un17@gmail.com>
2019-11-15 14:12:52 -0800

    Initial Commit

---
+++ GoodWeather_MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather_MVVM
+//
+//  Created by DONGGUN LEE on 11/15/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
