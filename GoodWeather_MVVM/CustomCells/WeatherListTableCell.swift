31f0866773a2e7cf0791d2e5696c39e22f17d41a
DONGGUN LEE <d0n99un17@gmail.com>
2019-11-15 15:14:40 -0800

    Displaying Hard-Coded Weather into TableView

---
+++ GoodWeather_MVVM/CustomCells/WeatherListTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherListTableCell.swift
+//  GoodWeather_MVVM
+//
+//  Created by DONGGUN LEE on 11/15/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListTableCell:UITableViewCell{
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+}
